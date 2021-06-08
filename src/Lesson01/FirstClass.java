17304fc5235b9bbe0345744ee762fc716563453b
Алексей Изюров <reamaer@list.ru>
2021-06-08 22:33:57 +0400

    Initial commit

---
+++ src/Lesson01/FirstClass.java
@@ -0,0 +1,13 @@
+package Lesson01;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        public static void printThreeWords(){
+            Sistem.out.println()
+        }
+
+    }
+
+}
