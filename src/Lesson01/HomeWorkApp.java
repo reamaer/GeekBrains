d35058f7ab1a7466fc9dceadf0c6fee8303d899f
Алексей Изюров <reamaer@list.ru>
2021-06-09 01:20:08 +0400

    HT № 1

---
+++ src/Lesson01/HomeWorkApp.java
@@ -0,0 +1,30 @@
+package Lesson01;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+
+        printThreeWords();
+
+        checkSumSign();
+    }
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int  a;
+        int b;
+
+        if (a + b >= 0) {
+            else {
+                System.out.println("Сумма Отрицательная");
+            }
+        }
+    }
+
+
+}
+
